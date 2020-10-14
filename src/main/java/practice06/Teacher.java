56ba3f1fca903bc6cbf3b89dcf35a2f62782f319
BORBECH <charlieborbs18@gmail.com>
2020-10-14 17:18:52 +0800

    [Practice-06][Charlie Borbe] feature: Added implementation to answer Practice-06 of OOP

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String introduce;
+        if (getKlass() == null){
+            introduce = super.introduce()+ " I am a Teacher. I teach No Class.";
+        }
+        else {
+            introduce = super.introduce()+ " I am a Teacher. I teach Class 2.";
+        }
+        return introduce;
+    }
 }
