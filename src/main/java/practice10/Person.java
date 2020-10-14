fd8c557a99d8d4c9c4d67bc7c1c0d0b02cf341d2
BORBECH <charlieborbs18@gmail.com>
2020-10-14 22:31:53 +0800

    [Practice-10][Charlie Borbe] feature: Added implementation to answer Practice-10 of OOP - 9 more test

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,46 @@
 package practice10;
 
+import java.util.Objects;
+
 public class Person {
+    private Integer id;
+    private String name;
+    private int age;
+
+    public Person(Integer id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() &&
+                getId().equals(person.getId()) &&
+                getName().equals(person.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getAge());
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,33 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    Klass klass;
+    LinkedList<Klass> klassLinkedList;
+
+    public Teacher(Integer id, String name, int age, LinkedList<Klass> klassLinkedList) {
+        super(id, name, age);
+        this.klassLinkedList = klassLinkedList;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public LinkedList<Klass> getKlassLinkedList() {
+        return klassLinkedList;
+    }
+
+    public String introduce() {
+        return getKlass() == null ?
+                super.introduce()+ " I am a Teacher. I teach No Class." :
+                super.introduce()+ " I am a Teacher. I teach Class 2.";
+    }
+
+    public String introduceWith(Student student){
+        return student.getKlass() == getKlass() ?
+                super.introduce()+" I am a Teacher. I teach "+student.getName()+"." :
+                super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+    }
 }
