16ff63edb4fc3b38600b310be75453ba45ff7f57
BORBECH <charlieborbs18@gmail.com>
2020-10-14 15:49:13 +0800

    [OOP][Charlie Borbe] feature: Added implementation to answer Practice 2 in OOP

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,21 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class 2.";
+    }
+
+
 }
